4cd914a5a089ddccae7e877a1739e50bff983a99
Jens Joy <jensjoy54@gmail.com>
2022-08-26 10:54:57 +0530

    new project

--- src/Product/data.js
+++ src/Product/data.js
@@ -2,24 +2,30 @@
 export const products=[
     {
         id:1,
+        img1:"dictionaryproject.png",
+        img2:"dictionaryppt.png",
+        link:"https://wonderful-cheesecake-2675ae.netlify.app"
+    },
+    {
+        id:2,
         img1:"FileManagerapk.png",
         img2:"FileMangerpresent.png",
         link:"https://festive-jennings-525104.netlify.app"
     },
     {
-        id:2,
+        id:3,
         img1:"Foodtruck.png",
         img2:"FoodTruckpresent.png",
         link:"https://github.com/learnwithjj/FOodTruck"
     },
     {
-        id:3,
+        id:4,
         img1:"ExpenseTracker.png",
         img2:"ExpenseTrackerpresent.png",
         link:"https://github.com/learnwithjj/Expense-Tracker"
     },
     {
-        id:4,
+        id:5,
         img1:"tictactoe.png",
         img2:"tictactoepresent.png",
         link:"https://github.com/learnwithjj/TicTacToe"
